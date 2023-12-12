ccf81753db44098616e6fd88333270bddb352c57
ysshwan147 <chick123159147@gmail.com>
2023-12-13 06:33:41 +0900

    add computer screen

--- Assets/Scripts/Objects/Computer/Computer.cs
+++ Assets/Scripts/Objects/Computer/Computer.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class Computer : MonoBehaviour
 {
+    public MeshRenderer mainScreen;
+    public MeshRenderer subScreen;
+
+    public Material offMat;
+    public Material loginMat;
+    public Material sylla1Mat;
+    public Material sylla2Mat;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        turnOffComputer();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void turnOnComputer()
     {
-        
+        mainScreen.material = loginMat;
+    }
+
+    public void turnOffComputer()
+    {
+        mainScreen.material = offMat;
+        subScreen.material = offMat;
+    }
+
+    public void loginComputer()
+    {
+        mainScreen.material = sylla1Mat;
+        subScreen.material = sylla2Mat;
     }
 }
--- Assets/Scripts/Objects/Switch/ToggleComputerPower.cs
+++ Assets/Scripts/Objects/Switch/ToggleComputerPower.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 public class ToggleComputerPower : MonoBehaviour
 {
     public Computer computer;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool isOn = false;
 
     public void SelectEntered()
     {
+        toggleComputer();
+    }
 
+    private void toggleComputer()
+    {
+        isOn = !isOn;
+
+        if (isOn)
+        {
+            computer.turnOnComputer();
+        }
+        else
+        {
+            computer.turnOffComputer();
+        }
     }
 }
