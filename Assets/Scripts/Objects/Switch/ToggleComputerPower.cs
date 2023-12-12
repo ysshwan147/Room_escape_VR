074ea0baa6af6236bb27a7d6f5dcd1825a82b653
ysshwan147 <chick123159147@gmail.com>
2023-12-13 03:16:16 +0900

    add computer switch

---
+++ Assets/Scripts/Objects/Switch/ToggleComputerPower.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ToggleComputerPower : MonoBehaviour
+{
+    public Computer computer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectEntered()
+    {
+
+    }
+}
