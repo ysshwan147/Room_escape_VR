a254caefa0d38e79c27fc14f34730cf2ad900131
ysshwan147 <chick123159147@gmail.com>
2023-12-14 11:51:47 +0900

    병합하면서 날라간거 추가

---
+++ Assets/Scripts/Objects/ComputerPowerSwitch/ToggleComputerPower.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleComputerPower : MonoBehaviour
+{
+    public Computer computer;
+    public bool isOn = false;
+
+    public void SelectEntered()
+    {
+        toggleComputer();
+    }
+
+
+    private void toggleComputer()
+    {
+        isOn = !isOn;
+
+        if (isOn)
+        {
+            computer.turnOnComputer();
+        }
+        else
+        {
+            computer.turnOffComputer();
+        }
+    }
+}
